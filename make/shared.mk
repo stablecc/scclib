e5ecd9cd0b2d885a4a8f198002648c001210ac0c
Mike Wilson <mike@stablecc.com>
2022-03-12 14:56:19 -0800

    Makefile overhaul

--- make/shared.mk
+++ make/shared.mk
@@ -1,3 +1,32 @@
+# BSD 3-Clause License
+# 
+# Copyright (c) 2022, Stable Cloud Computing, Inc.
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+# 
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+# 
+# 3. Neither the name of the copyright holder nor the names of its
+#    contributors may be used to endorse or promote products derived from
+#    this software without specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 # shared rules which must be included first in submakefiles
 
 .PHONY: default
@@ -19,18 +48,17 @@ endif
 
 # choose minimal flags to get the job done
 
-CPPFLAGS += -march=$(ARCH) -pipe
-CXXFLAGS += -std=c++17
+CXXFLAGS += -march=$(ARCH) -pipe -U_FORTIFY_SOURCE -fstack-protector -Wall -Wunused-but-set-parameter -Wno-free-nonheap-object -fno-omit-frame-pointer -std=c++17
+CPPFLAGS += -fno-canonical-system-headers -Wno-builtin-macro-redefined '-D__DATE__="redacted"' '-D__TIMESTAMP__="redacted"' '-D__TIME__="redacted"'
 LDFLAGS += -lstdc++ -pthread -lm
 
 # all intermediate output goes to objdir
 ifeq ($(BLDTYPE),debug)
-CPPFLAGS += -Wall -O0 -g
+CXXFLAGS += -O0 -g
 OBJDIR = $(BASE)/obj/$(NAME)_d
 else
 # NDEBUG is defined to disable any assert() macros in production
-# production uses maximum optimization, probably not recommended
-CPPFLAGS += -O3 -DNDEBUG
+CXXFLAGS += -g0 -O3 -DNDEBUG -ffunction-sections -fdata-sections
 OBJDIR = $(BASE)/obj/$(NAME)
 endif
 
